ee05659ef7df35f4b1a7bf98bafd8ff6dc04e158
fuer8768 <zhou_faith@163.com>
2016-08-01 16:53:11 +0800

    update

--- Hello.js
+++ Hello.js
@@ -3,5 +3,5 @@ http.createServer(function (request, response) {
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
-}).listen(8888,"127.0.0.1");
+}).listen(8080,"127.0.0.1");
 console.log('Server running at http://127.0.0.1:8888/');
